829d7c3c97d9adddc3505ab39bec198322328be7
kbuhantsev <k.buhantsev@gmail.com>
2024-01-03 17:58:45 +0200

    templates

--- djangoProject/main/views.py
+++ djangoProject/main/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request) -> HttpResponse:
+    context = {
+        "title":"Home",
+        "content":"Главная страница магазина - HOME",
+        "list": ["first", "second"],
+        "dict": {"first":"1"},
+        "bool": True
+
+    }
+    return render(request, 'main/index.html', context)
+
+
+def about(request) -> HttpResponse:
+    return HttpResponse(content="About page")
