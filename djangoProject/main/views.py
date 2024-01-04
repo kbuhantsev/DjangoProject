95ffc4fed08ccf1595d3007e86decbce0d5a04b1
kbuhantsev <k.buhantsev@gmail.com>
2024-01-04 17:05:41 +0200

    fixtures and goods

--- djangoProject/main/views.py
+++ djangoProject/main/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from goods.models import Categories
+
 
 # Create your views here.
 def index(request) -> HttpResponse:
+    categories = Categories.objects.all()
+
     context = {
-        "title":"Home - Главная",
-        "content":"Магазин мебели HOME",
+        "title": "Home - Главная",
+        "content": "Магазин мебели HOME",
+        "categories": categories,
     }
-    return render(request, 'main/index.html', context)
+    return render(request, "main/index.html", context)
 
 
 def about(request) -> HttpResponse:
     context = {
-        "title":"Home - О нас",
-        "content":"О нас",
-        "text_on_page": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi qui iure reprehenderit. Exercitationem sequi consequatur cupiditate deleniti nobis! Provident eius facilis a, quisquam laudantium aspernatur odit tenetur error at possimus architecto totam necessitatibus nam fugiat odio ex. Impedit ea, beatae dicta fugiat quos ab, deleniti temporibus quam quaerat, recusandae aperiam!"
+        "title": "Home - О нас",
+        "content": "О нас",
+        "text_on_page": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi qui iure reprehenderit. Exercitationem sequi consequatur cupiditate deleniti nobis! Provident eius facilis a, quisquam laudantium aspernatur odit tenetur error at possimus architecto totam necessitatibus nam fugiat odio ex. Impedit ea, beatae dicta fugiat quos ab, deleniti temporibus quam quaerat, recusandae aperiam!",
     }
-    return render(request, 'main/about.html', context)
+    return render(request, "main/about.html", context)
