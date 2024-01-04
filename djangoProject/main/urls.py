5dba6a86daf9cd1748c304bef14ccc9c26c614e4
kbuhantsev <k.buhantsev@gmail.com>
2024-01-04 12:47:35 +0200

    templates

--- djangoProject/main/urls.py
+++ djangoProject/main/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 
+from django.urls import URLPattern, path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main'))
+from main import views
+
+app_name = 'main' # pylint: disable=invalid-name
+
+urlpatterns: list[URLPattern] = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about')
 ]
