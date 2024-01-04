ff1ad8fcee667ce3b290728293e2cae911dcfdab
kbuhantsev <k.buhantsev@gmail.com>
2024-01-04 17:15:40 +0200

    debug toolbar

--- djangoProject/app/urls.py
+++ djangoProject/app/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import URLResolver, path, include
 
+from app.settings import DEBUG
+
 
 urlpatterns: list[URLResolver] = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main')),
-    path('catalog/', include('goods.urls', namespace='catalog'))
+    path("admin/", admin.site.urls),
+    path("", include("main.urls", namespace="main")),
+    path("catalog/", include("goods.urls", namespace="catalog")),
 ]
+
+if DEBUG:
+    urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
