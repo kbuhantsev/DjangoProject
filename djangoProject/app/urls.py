900bb981b916bc377e02fcc50a8c162a1f46c738
kbuhantsev <k.buhantsev@gmail.com>
2024-01-04 12:28:13 +0200

    static

--- djangoProject/app/urls.py
+++ djangoProject/app/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from djangoProject.main import views
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- djangoProject/main/views.py
+++ djangoProject/main/views.py
@@ -5,15 +5,16 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request) -> HttpResponse:
     context = {
-        "title":"Home",
-        "content":"Главная страница магазина - HOME",
-        "list": ["first", "second"],
-        "dict": {"first":"1"},
-        "bool": True
-
+        "title":"Home - Главная",
+        "content":"Магазин мебели HOME",
     }
     return render(request, 'main/index.html', context)
 
 
 def about(request) -> HttpResponse:
-    return HttpResponse(content="About page")
+    context = {
+        "title":"Home - О нас",
+        "content":"О нас",
+        "text_on_page": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi qui iure reprehenderit. Exercitationem sequi consequatur cupiditate deleniti nobis! Provident eius facilis a, quisquam laudantium aspernatur odit tenetur error at possimus architecto totam necessitatibus nam fugiat odio ex. Impedit ea, beatae dicta fugiat quos ab, deleniti temporibus quam quaerat, recusandae aperiam!"
+    }
+    return render(request, 'main/about.html', context)
