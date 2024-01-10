14eb7b60ecebd6c8fb0b220a0a6aacac85491c82
kbuhantsev <k.buhantsev@gmail.com>
2024-01-10 18:02:48 +0200

    registration form

--- djangoProject/users/views.py
+++ djangoProject/users/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 # Create your views here.
 
@@ -26,6 +26,18 @@ def login(request):
 
 
 def registration(request):
+    # if request.method == "POST":
+    #     form = UserRegistrationForm(data=request.POST)
+    #     if form.is_valid():
+    #         username = request.POST["username"]
+    #         password = request.POST["password"]
+    #         user = auth.authenticate(username=username, password=password)
+    #         if user:
+    #             auth.login(request, user)
+    #             return HttpResponseRedirect(reverse('main:index'))
+    # else:
+    #     form = UserLoginForm()
+    
     context = {"title": "Home - Регистрация"}
     return render(request, "users/registration.html", context)
 
