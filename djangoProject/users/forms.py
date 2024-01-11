0d867dcd173d473c269743c7a7624b51d55aebde
kbuhantsev <k.buhantsev@gmail.com>
2024-01-11 12:46:04 +0200

    profile

--- djangoProject/users/forms.py
+++ djangoProject/users/forms.py
@@ -1,5 +1,9 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    UserChangeForm,
+    UserCreationForm,
+)
 from users.models import User
 
 
@@ -51,3 +55,21 @@ class UserLoginForm(AuthenticationForm):
     #         }
     #     )
     # )
+
+
+class ProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = [
+            "image",
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+        ]
+
+    image = forms.ImageField(widget=forms.FileInput(), required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
