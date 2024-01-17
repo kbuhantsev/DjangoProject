6264b6e69cd5ebd9f04a78674e2a22469366bb33
kbuhantsev <k.buhantsev@gmail.com>
2024-01-17 17:04:53 +0200

    Update models.py

--- djangoProject/carts/models.py
+++ djangoProject/carts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+from goods.models import Products
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    product = models.ForeignKey(to=Products, on_delete=models.CASCADE, verbose_name="Продукт")
+    quantity = models.IntegerField(default=0, verbose_name="Количество")
+    created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name="Дата добавления")
+
+    class Meta:
+        verbose_name = "Корзина"
+        verbose_name_plural = "Корзина"
+
+    def __str__(self):
+        return f"Корзина {self.user.username} | Товар {self.product.name} | Количество {self.quantity}"
