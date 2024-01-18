013e58ad2dc598cf93b0ba2bc48ff1a06d6971a7
kbuhantsev <k.buhantsev@gmail.com>
2024-01-18 16:25:33 +0200

    ajax change quantity

--- djangoProject/carts/views.py
+++ djangoProject/carts/views.py
@@ -35,7 +35,27 @@ def cart_add(request):
 
 
 def cart_change(request):
-    return None
+    cart_id = request.POST.get('cart_id')
+    quantity = request.POST.get('quantity')
+
+    cart = Cart.objects.get(id=cart_id)
+    cart.quantity = quantity
+    cart.save()
+
+    updated_quantity = cart.quantity
+
+    cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        template_name="carts/includes/included_cart.html", context={"carts": cart}, request=request
+    )
+
+    response_data = {
+        "message": "Количество изменено",
+        "cart_items_html": cart_items_html,
+        "quantity": updated_quantity
+    }
+
+    return JsonResponse(response_data)
 
 
 def cart_remove(request):
@@ -51,7 +71,7 @@ def cart_remove(request):
     )
 
     response_data = {
-        "message": "Товар добавлен в корзину",
+        "message": "Товар удален из корзины",
         "cart_items_html": cart_items_html,
         "quantity_deleted": quantity
     }
