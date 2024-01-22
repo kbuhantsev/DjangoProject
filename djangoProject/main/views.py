29a836810d5314e2338ed714ad0bf55cc89118c5
kbuhantsev <k.buhantsev@gmail.com>
2024-01-22 11:00:08 +0200

    final commit

--- djangoProject/main/views.py
+++ djangoProject/main/views.py
@@ -15,6 +15,10 @@ def about(request) -> HttpResponse:
     context = {
         "title": "Home - О нас",
         "content": "О нас",
-        "text_on_page": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi qui iure reprehenderit. Exercitationem sequi consequatur cupiditate deleniti nobis! Provident eius facilis a, quisquam laudantium aspernatur odit tenetur error at possimus architecto totam necessitatibus nam fugiat odio ex. Impedit ea, beatae dicta fugiat quos ab, deleniti temporibus quam quaerat, recusandae aperiam!",
+        "text_on_page": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi qui iure reprehenderit. "
+        "Exercitationem sequi consequatur cupiditate deleniti nobis! Provident eius facilis a, "
+        "quisquam laudantium aspernatur odit tenetur error at possimus architecto totam "
+        "necessitatibus nam fugiat odio ex. Impedit ea, beatae dicta fugiat quos ab, "
+        "deleniti temporibus quam quaerat, recusandae aperiam!",
     }
     return render(request, "main/about.html", context)
