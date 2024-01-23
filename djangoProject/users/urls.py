9c6b8e3a0293a9d665568d4f5a0590eadee2ec76
kbuhantsev <k.buhantsev@gmail.com>
2024-01-23 09:54:43 +0200

    passw reser forms

--- djangoProject/users/urls.py
+++ djangoProject/users/urls.py
@@ -8,7 +8,7 @@ from django.urls import URLPattern, path, reverse_lazy
 
 from users import views
 
-app_name = "users"  # pylint: disable=invalid-name
+app_name = "users"
 
 urlpatterns: list[URLPattern] = [
     path("login/", views.login, name="login"),
@@ -20,29 +20,31 @@ urlpatterns: list[URLPattern] = [
     path(
         "password-reset/",
         PasswordResetView.as_view(
-            template_name="users/password_reset.html",
-            email_template_name="users/password_reset_email.html",
+            template_name="users/password_reset/password_reset.html",
+            email_template_name="users/password_reset/password_reset_email.html",
             success_url=reverse_lazy("users:password_reset_done"),
         ),
         name="password_reset",
     ),
     path(
         "password-reset/done/",
-        PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
+        PasswordResetDoneView.as_view(
+            template_name="users/password_reset/password_reset_done.html"
+        ),
         name="password_reset_done",
     ),
     path(
         "password-reset/reset/<uidb64>/<token>/",
         PasswordResetConfirmView.as_view(
-            template_name="users/password_reset_confirm.html",
-            success_url=reverse_lazy('users:password_reset_complete')
+            template_name="users/password_reset/password_reset_confirm.html",
+            success_url=reverse_lazy("users:password_reset_complete"),
         ),
         name="password_reset_confirm",
     ),
     path(
         "password-reset/reset/done/",
         PasswordResetCompleteView.as_view(
-            template_name="users/password_reset_complete.html"
+            template_name="users/password_reset/password_reset_complete.html"
         ),
         name="password_reset_complete",
     ),
